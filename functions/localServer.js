b4d82dfaf496b714307b80dcdcba062d38570ef7
Matijasevic-Emer <matijasevic.emerson@gmail.com>
2025-03-24 15:53:44 -0300

    control

--- functions/localServer.js
+++ functions/localServer.js
@@ -1,26 +1,26 @@
-import express from "express";
-import cors from "cors";
-import { fetchUsers, addUser, fetchUserById, fetchUserByUID, updateUserById, deleteUserById } from "./controllers/userController.js";
+// import express from "express";
+// import cors from "cors";
+// import { fetchUsers, addUser, fetchUserById, fetchUserByUID, updateUserById, deleteUserById } from "./controllers/userController.js";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
-const port = process.env.PORT || 4006;
-const router = express.Router();
+// const port = process.env.PORT || 4006;
+// const router = express.Router();
 
-// Rutas de usuarios
-router.get("/users", fetchUsers); // Obtener usuarios por teamId
-router.get("/users/:userId", fetchUserById); // Obtener un usuario por ID
-router.get("/users/uid/:UID", fetchUserByUID); // Obtener un usuario por UID
-router.post("/users", addUser); // Crear un nuevo usuario
-router.put("/users/:userId", updateUserById); // Actualizar un usuario por ID
-router.delete("/users/:userId", deleteUserById); // Eliminar un usuario por ID
+// // Rutas de usuarios
+// router.get("/users", fetchUsers); // Obtener usuarios por teamId
+// router.get("/users/:userId", fetchUserById); // Obtener un usuario por ID
+// router.get("/users/uid/:UID", fetchUserByUID); // Obtener un usuario por UID
+// router.post("/users", addUser); // Crear un nuevo usuario
+// router.put("/users/:userId", updateUserById); // Actualizar un usuario por ID
+// router.delete("/users/:userId", deleteUserById); // Eliminar un usuario por ID
 
-// Usa las rutas para tareas
-app.use('/api', router);
+// // Usa las rutas para tareas
+// app.use('/api', router);
 
-// Servidor local para tareas
-app.listen(port, () => {
-    console.log(`Tasks Service is running on http://localhost:${port}`);
-});
+// // Servidor local para tareas
+// app.listen(port, () => {
+//     console.log(`Tasks Service is running on http://localhost:${port}`);
+// });
--- functions/models/userModel.js
+++ functions/models/userModel.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, getDoc, addDoc, updateDoc, deleteDoc, query, where, doc } from "firebase/firestore";
+import dotenv from 'dotenv';
+dotenv.config();
 
-// Configura Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCx8GI5km0guJojFuOb9KDKNSclqFQBhLI",
-    authDomain: "taskban-v1.firebaseapp.com",
-    projectId: "taskban-v1",
-    storageBucket: "taskban-v1.appspot.com",
-    messagingSenderId: "774075443466",
-    appId: "1:774075443466:web:0b1ccf90595264ef8872f3",
-    measurementId: "G-1MCX6F9W86"
+    apiKey: process.env.VITE_API_KEY,
+    authDomain: process.env.VITE_AUTH_DOMAIN,
+    projectId: process.env.VITE_PROJECT_ID,
+    storageBucket: process.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_APP_ID,
+    measurementId: process.env.VITE_MEASUREMENT_ID,
 };
 
 // Inicializa Firebase y Firestore
